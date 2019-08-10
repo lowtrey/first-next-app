92cfbf67ed5076e7f1e0e0e96542bdaef796216e
Q. Price <priceqp@gmail.com>
2019-08-10 16:58:59 -0500

    Test Next.js routing

---
+++ pages/about.js
@@ -0,0 +1,5 @@
+const AboutPage = () => {
+  return <h1>THIS IS THE ABOUT PAGE...</h1>;
+};
+
+export default AboutPage;
---
+++ pages/contact.js
@@ -0,0 +1,8 @@
+const ContactPage = () => (
+  <div>
+    <h1>Contact Page</h1>
+    <p>Do not contact us...ever</p>
+  </div>
+);
+
+export default ContactPage;
