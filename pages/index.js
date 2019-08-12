75676b39f49c928be4b45667c1ed473379623274
Q. Price <priceqp@gmail.com>
2019-08-12 11:19:29 -0500

    Install axios, test API fetch on index component

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,21 @@
-const Index = () => {
+import axios from 'axios';
+
+const Index = ({ posts }) => {
   return (
     <div>
       <h1>Our Index Page!!!</h1>
+      <ul>
+      {posts.map(post => (
+        <li key={post.id}>{post.title}</li>
+      ))}
+      </ul>
     </div>
   );
 };
 Index.getInitialProps = async () => {
-  console.log('GET INITIAL PROPS!!');
+  const res = await axios.get('https://jsonplaceholder.typicode.com/posts');
+  const { data } = res;
+  return { posts: data };
 };
 
 export default Index;
